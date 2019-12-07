d23ac6df45fd278c2c6e19c06b6db69a7edb1406
Daniel Johann Sept <daniel.sept@alumni.fh-aachen.de>
2019-12-07 21:10:51 +0100

    auth.php sets user and pseudorole

--- Auth.php
+++ Auth.php
@@ -14,27 +14,30 @@ else {
     echo 'beide variablen wurden geliefert </br> ';
     echo $_POST['password'] . ' user ' . $_POST['user'];
     $_SESSION['password'] = $_POST['password'];
-    $_SESSION['user'] = $_POST['user'];
+
 
 
     //check if pw ist korrekt
-    $query = 'SELECT Nutzername, `Hash1` FROM Benutzer where Nutzername=\''.$_POST['user'].'\';'; // Ihre SQL Query aus HeidiSQL
+    $query = 'SELECT Nutzername, `Hash1`,Vorname FROM Benutzer where Nutzername=\''.$_POST['user'].'\';'; // Ihre SQL Query aus HeidiSQL
     $link = mysqli_connect(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASS'), getenv('DB_NAME'), getenv('DB_PORT'));
 
     if (mysqli_connect_errno()) {
         printf("Konnte nicht zur entfernten Datenbank verbinden: %s\n", mysqli_connect_error());
         exit();
     }
-    $nutzernamenarray = array();
+    //$nutzernamenarray = array();
     if ($result = mysqli_query($link, $query)) {
         //var_dump( $result->num_rows);
         //echo $result->num_rows;
         while ($row = mysqli_fetch_assoc($result)) {
         //hier kommen wir nur rein wenn user existiert
         echo 'user existiert';
-        if(password_verify($_SESSION['password'],$row['Hash1']))
+        if(password_verify($_POST['password'],$row['Hash1']))
         {
             echo 'passwort ist korrekt';
+            $_SESSION['user']=$row['Vorname'];
+            $_SESSION['role']='hierkommtrollehin';
+
         }
 
         }
