7ede93b4b85a0670bf2f91f39a5a03ea761c45e6
Daniel Johann Sept <daniel.sept@alumni.fh-aachen.de>
2019-12-12 21:46:02 +0100

    details is set in view, little changes still to do

--- DetailBlade.php
+++ DetailBlade.php
@@ -4,6 +4,17 @@ require "vendor/autoload.php";
 /*
  *	BladeOne Viewengine aufsetzen
  */
+//notwendiges für datenbankaufbau
+
+if (!isset($_SESSION)) session_start();
+require __DIR__ . '/vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::create(__DIR__, '.env');
+$dotenv->load();
+$dotenv->required(['DB_HOST', 'DB_NAME', 'DB_USER', 'DB_PASS', 'DB_PORT']);
+
+
+
+
 
 use eftec\bladeone\BladeOne;
 
@@ -18,7 +29,52 @@ $blade = new BladeOne($views,$cache,BladeOne::MODE_AUTO);
  * übergeben werden können
  */
 
-$title = "Views Demo";
+$übergebeneID = -1;
+if (isset($_GET["id"])) {
+    $übergebeneID = $_GET["id"];
+}
+//query stellen
+$query = 'SELECT Mahlzeiten.`Name`, Mahlzeiten.ID, Beschreibung, Vorrat,
+Kategorie, hatBilder.MahlzeitenID,Jahr, Gastpreis, 
+Studentpreis,
+`MA-Preis`, BilderID, `Alt-Text`, `Binärdaten`, Titel, ZutatenID ,Zutaten.Name AS Zutatenname
+
+FROM Mahlzeiten 
+left JOIN Preise ON Mahlzeiten.id=Preise.MahlzeitenID  
+left join hatBilder on Mahlzeiten.ID   = hatBilder.MahlzeitenID 
+left JOIN Bilder ON hatBilder.BilderID = Bilder.ID
+left Join enthältZutaten on Mahlzeiten.ID=enthältZutaten.MahlzeitenID
+left Join Zutaten on enthältZutaten.ZutatenID=Zutaten.ID
+WHERE Mahlzeiten.id=' . $übergebeneID . ';'; // Ihre SQL Query aus HeidiSQL
+
+
+$link = mysqli_connect(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASS'), getenv('DB_NAME'), getenv('DB_PORT'));
+
+if (mysqli_connect_errno()) {
+    printf("Konnte nicht zur entfernten Datenbank verbinden: %s\n", mysqli_connect_error());
+    exit();
+}
+
+$result = mysqli_query($link, $query);
+$result2 = mysqli_query($link, $query);
+//gucken ob id gesetzt ist
+$row = mysqli_fetch_assoc($result);
+
+//$title = "Views Demo";
+
+
+$zutatenrows=array();
+while ($row2 = mysqli_fetch_assoc($result2)) {
+    if (isset($row2['Zutatenname'])) {
+
+        $zutatenrows[]=$row2;
+    }
+
+}
+mysqli_close($link);
+
+
+
 
 $defs = array();
 array_push($defs,array("label" => "PRIMARY KEY", "term" => "Primärschlüssel"));
@@ -71,9 +127,9 @@ array_push($labels, "Stéphane Mingot @smingot");
 
 //echo "<footer><p>Sie sehen, mit der View-Engine können Sie jede Menge Code auslagern und in den dedizierten Views eine spezialisierte Syntax verwenden.</p></footer>";
 
-echo $blade->run("pages.detail",array("hallo"=>$defs,'title'=>$title));
+echo $blade->run("pages.detail",array("result"=>$result,"row"=>$row,'zutatenrows'=>$zutatenrows));
 
-
+//
 //echo "
 //		</div>
 //	</body>
--- DetailCopyzurBearbeitung.php
+++ DetailCopyzurBearbeitung.php
@@ -2,50 +2,26 @@
     <!-- hier testen wir ob die Informationen der Übergebenen ID vorhanden sind -->
     <?php
 
-    $übergebeneID = -1;
-    if (isset($_GET["id"])) {
-        $übergebeneID = $_GET["id"];
-    }
-
-    //echo $übergebeneID;
-    $query = 'SELECT Mahlzeiten.`Name`, Mahlzeiten.ID, Beschreibung, Vorrat,
-Kategorie, hatBilder.MahlzeitenID,Jahr, Gastpreis, 
-Studentpreis,
-`MA-Preis`, BilderID, `Alt-Text`, `Binärdaten`, Titel, ZutatenID ,Zutaten.Name AS Zutatenname
-
-FROM Mahlzeiten 
-left JOIN Preise ON Mahlzeiten.id=Preise.MahlzeitenID  
-left join hatBilder on Mahlzeiten.ID   = hatBilder.MahlzeitenID 
-left JOIN Bilder ON hatBilder.BilderID = Bilder.ID
-left Join enthältZutaten on Mahlzeiten.ID=enthältZutaten.MahlzeitenID
-left Join Zutaten on enthältZutaten.ZutatenID=Zutaten.ID
-WHERE Mahlzeiten.id=' . $übergebeneID . ';'; // Ihre SQL Query aus HeidiSQL
+    //id setzen
 
 
-    $link = mysqli_connect(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASS'), getenv('DB_NAME'), getenv('DB_PORT'));
 
-    if (mysqli_connect_errno()) {
-        printf("Konnte nicht zur entfernten Datenbank verbinden: %s\n", mysqli_connect_error());
-        exit();
-    }
+    //echo $query;
 
 
-    $result = mysqli_query($link, $query);
-    $result2 = mysqli_query($link, $query);
-    //gucken ob id gesetzt ist
-    $row = mysqli_fetch_assoc($result);
+    // else{echo 'lief wohl gut';}
+
+  //result 1 und 2
+    //row setzen
     if (isset($_GET["id"])) {
 
         if ($übergebeneID == $row['MahlzeitenID']) {
 
-
         }
         if ($übergebeneID != $row['MahlzeitenID']) {
-
-
             echo '<meta content="3; url=./Start.php" http-equiv="refresh">';
         }
-
+        //wen id gar nicht gesetzt ist
     } else {
         echo 'offenbar ist die id nicht gesetzt';
         echo '<meta content="3; url=./Start.php" http-equiv="refresh">';
@@ -53,26 +29,31 @@ WHERE Mahlzeiten.id=' . $übergebeneID . ';'; // Ihre SQL Query aus HeidiSQL
 
 
 
-    mysqli_close($link);
+   //link schließen
     ?>
-    <!-- <meta content="3; url=./Start.php" http-equiv="refresh"> -->
-    <link href="fontawesome-free-5.11.2-web/css/all.css" rel="stylesheet">
+//hier war ein header
 
-    <!-- Bootstrap CSS -->
-    <link crossorigin="anonymous" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
-          integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" rel="stylesheet">
-    <!--stylesheet-->
-    <link href="allgemeinStyle.css" rel="stylesheet">
-    <title>Detail <?php echo $row['Name']; ?></title>
-</head>
-<body>
-<div class="container">
-    <?php include('snippets/NavOben.php'); ?>
 
     <!--main part-->
     <div class="row" style="margin-top: 3em;">
 
-        @include('layouts.svlogin')
+        <!--form für login-->
+        <div class="col-3 ">
+
+            <?php
+
+            //var_dump($_SESSION);
+            if (isset($_SESSION['auth']) and $_SESSION['auth'] == true) {
+                //     echo 'hallo';
+                include('snippets/logout.php');
+            } else {
+                include('snippets/login.php');
+
+            }
+            ?>
+            <!--  echo '<meta content="3; url=./Detail.php?ID=1" http-equiv="refresh">'; -->
+
+        </div>
 
         <!--falafelbild und preise-->
 
@@ -307,7 +288,5 @@ WHERE Mahlzeiten.id=' . $übergebeneID . ';'; // Ihre SQL Query aus HeidiSQL
         </div>
 
     </div>
-
-
 
 
