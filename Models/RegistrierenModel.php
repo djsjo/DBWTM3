e140d1121562dcdc9a96acad846d609458de280d
Daniel Johann Sept <daniel.sept@alumni.fh-aachen.de>
2019-12-16 09:09:33 +0100

    close link on different place

--- Models/RegistrierenModel.php
+++ Models/RegistrierenModel.php
@@ -11,39 +11,7 @@ class RegistrierenModel
 
 
 
-    public static function zutaten($id=0)
-    {
-        $link = mysqli_connect(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASS'), getenv('DB_NAME'), getenv('DB_PORT'));
-        if (mysqli_connect_errno()) {
-            printf("Konnte nicht zur entfernten Datenbank verbinden: %s\n", mysqli_connect_error());
-            exit();
-        } else {
-            //echo 'lief wohl gut';
-        }
-        $query = 'SELECT ID,Name,Bio,Vegan, Vegetarisch,Glutenfrei FROM Zutaten order by Bio desc,Name asc;'; // Ihre SQL Query aus HeidiSQL
 
-
-        $result = mysqli_query($link, $query);
-//$result2 = mysqli_query($link, $query);
-//gucken ob id gesetzt ist
-//$row = mysqli_fetch_assoc($result);
-
-//$title = "Views Demo";
-
-
-        $zutatenrows = array();
-        while ($row = mysqli_fetch_assoc($result)) {
-            if (isset($row)) {
-
-                $zutatenrows[] = $row;
-            }
-
-        }
-
-
-        mysqli_close($link);
-        return $zutatenrows;
-    }
     public static function mailExits($mailadress){
         $link = mysqli_connect(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASS'), getenv('DB_NAME'), getenv('DB_PORT'));
         if (mysqli_connect_errno()) {
@@ -56,14 +24,14 @@ class RegistrierenModel
 
 
         $result = mysqli_query($link, $query);
-
+        mysqli_close($link);
         if($result->num_rows>0)
         {
             return true;
         }
         else return false;
 
-        mysqli_close($link);
+
     }
     public static function matrNrExits($matrikelnr){
         $link = mysqli_connect(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASS'), getenv('DB_NAME'), getenv('DB_PORT'));
@@ -77,14 +45,14 @@ class RegistrierenModel
 
 
         $result = mysqli_query($link, $query);
-
+        mysqli_close($link);
         if($result->num_rows>0)
         {
             return true;
         }
         else return false;
 
-        mysqli_close($link);
+
     }
     public static function matrNrInRange($matrikelnr){
         $link = mysqli_connect(getenv('DB_HOST'), getenv('DB_USER'), getenv('DB_PASS'), getenv('DB_NAME'), getenv('DB_PORT'));
@@ -98,13 +66,13 @@ class RegistrierenModel
 
 
         $result = mysqli_query($link, $query);
-
+        mysqli_close($link);
         if($result->num_rows>0)
         {
             return true;
         }
         else return false;
 
-        mysqli_close($link);
+
     }
 }
